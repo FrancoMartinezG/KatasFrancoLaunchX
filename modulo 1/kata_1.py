9bca90070451331eb6bd5a0e227ff8835933ad12
FrancoMartinezG <franco.a.mtzgcia@hotmail.com>
2022-02-09 21:10:13 -0600

    Se agregan katas de la 0 a la 6

---
+++ modulo 1/kata_1.py
@@ -0,0 +1,11 @@
+## Fecha del dia de hoy
+
+from datetime import date
+
+print("La fecha de hoy es: " + str(date.today()))
+
+## Converitor de Unidades
+
+parsec = 11
+lightyear = 3.26156 * parsec
+print(str(parsec) + " parsec, son " + str(lightyear) + " años luz")
